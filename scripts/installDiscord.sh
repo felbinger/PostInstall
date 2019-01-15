1c1a4cf47e1b56f10c27c7462bd4a9b67e84ea6a
Nico Felbinger <nico.felbinger2000@gmail.com>
2019-01-15 18:49:13 +0100

    Updated installDiscord.sh to version 0.0.6

--- scripts/installDiscord.sh
+++ scripts/installDiscord.sh
@@ -3,8 +3,8 @@
 if [ `/usr/bin/id -u` != "0" ]; then
   echo "Please run the script as root!"
 else
-  wget -q https://dl.discordapp.net/apps/linux/0.0.5/discord-0.0.5.deb
-  dpkg -i discord-0.0.5.deb
-  rm discord-0.0.5.deb
+  wget -q https://dl.discordapp.net/apps/linux/0.0.6/discord-0.0.6.deb
+  dpkg -i discord-0.0.6.deb
+  rm discord-0.0.6.deb
   apt install -f -y
 fi

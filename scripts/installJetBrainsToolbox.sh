14ed0cc94749a8ee39fa677281e776ab04e6f3c0
Nico Felbinger <nico.felbinger2000@gmail.com>
2019-05-03 13:42:05 +0200

    updated burp, telegram and jetbrains toolbox

--- scripts/installJetBrainsToolbox.sh
+++ scripts/installJetBrainsToolbox.sh
@@ -5,7 +5,7 @@
 if [ `/usr/bin/id -u` != "0" ]; then
   echo "Please run the script as root!"
 else
-  wget -O- "https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.12.4481.tar.gz" | tar xz
+  wget -O- "https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.14.5179.tar.gz" | tar xz
   mv jetbrains-toolbox-1.12.4481/jetbrains-toolbox /usr/bin
   rm -rf jetbrains-toolbox-1.12.4481
 fi
--- scripts/installTelegram.sh
+++ scripts/installTelegram.sh
@@ -5,7 +5,7 @@
 if [ `/usr/bin/id -u` != "0" ]; then
   echo "Please run the script as root!"
 else
-  wget -qO- https://updates.tdesktop.com/tlinux/tsetup.1.2.17.tar.xz | tar xJC /opt/
+  wget -qO- "https://updates.tdesktop.com/tlinux/tsetup.1.6.7.tar.xz" | tar xJC /opt/
   ln -s /opt/Telegram/Telegram /usr/bin/telegram
   telegram &
 fi

7087e5161b461d2d57c4e1c5cff5d1491518bd47
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2020-11-08 21:31:43 +0100

    Update installJetBrainsToolbox.sh

--- scripts/installJetBrainsToolbox.sh
+++ scripts/installJetBrainsToolbox.sh
@@ -3,7 +3,7 @@
 if [[ $(/usr/bin/id -u) != "0" ]]; then
   echo "Please run the script as root!"
 else
-  wget -O- "https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.17.7391.tar.gz" | tar xz
+  wget -O- "https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.18.7455.tar.gz" | tar xz
   mv jetbrains-toolbox-1.12.4481/jetbrains-toolbox /usr/bin
   rm -rf jetbrains-toolbox-1.12.4481
 fi

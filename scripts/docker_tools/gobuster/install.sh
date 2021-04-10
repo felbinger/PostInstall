1843298f3cdc5f77d2e5a082cf1badc5dcb151ae
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2021-04-10 03:06:49 +0200

    Update install.sh

--- scripts/docker_tools/gobuster/install.sh
+++ scripts/docker_tools/gobuster/install.sh
@@ -11,24 +11,32 @@ WORDLISTS=(
   "https://raw.githubusercontent.com/diasdavid/node-dirbuster/master/lists/apache-user-enum-2.0.txt"
 )
 
-WORDLIST_DIR=$(cat gobuster.sh | grep "WORDLIST_DIR=" | cut -d "=" -f 2)
+WORDLIST_DIR=$(cat $(pwd)/gobuster.sh | grep "WORDLIST_DIR=" | cut -d "=" -f 2)
 WORDLIST_DIR=${WORDLIST_DIR//\"}
 
 read -p "Which directory should be used for the wordlists? (Default: ${WORDLIST_DIR}): " IN_WORDLIST_DIR
+
+# set directory where the wordlists are located
+if [[ ! -z ${IN_WORDLIST_DIR} ]]; then
+  sed -i "s#${WORDLIST_DIR}#${IN_WORDLIST_DIR}#g" $(pwd)/gobuster.sh
+  WORDLIST_DIR=${IN_WORDLIST_DIR}
+fi
+
 read -p "Should the most useful wordlists be downloaded? (Y/n): " GET_WORDLISTS
 
+# download recommended wordlists
 if [[ -z ${GET_WORDLISTS} ]] || [[ ${GET_WORDLISTS} == "Y" ]] || [[ ${GET_WORDLISTS} == "y" ]]; then
   if [[ -z $(which wget) ]]; then
     sudo apt install -y wget
   fi
   for url in ${WORDLISTS[@]}; do
-    wget -q $url -P $WORDLIST_DIR
+    if [[ ! -f "${WORDLIST_DIR}/${url##*/}" ]]; then
+      wget -q ${url} -P ${WORDLIST_DIR}
+    fi
   done
 fi
 
-if [[ ! -z ${IN_WORDLIST_DIR} ]]; then
-  sed -i "s#${WORDLIST_DIR}#${IN_WORDLIST_DIR}#g" ./gobuster.sh
+sudo chmod +x $(pwd)/gobuster.sh
+if [ ! -h /usr/local/bin/gobuster ]; then
+  sudo ln -s $(pwd)/gobuster.sh /usr/local/bin/gobuster
 fi
-
-sudo cp ./gobuster.sh /usr/local/bin/gobuster
-sudo chmod +x /usr/local/bin/gobuster
